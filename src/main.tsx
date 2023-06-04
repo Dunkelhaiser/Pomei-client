530870ced6bb9c56e85a891fa6b3f54db69009be
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-06-04 14:33:23 +0300

    Disabled devtools for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 import NotesContextProvider from "./context/NotesContext";
 import UserContextProvider from "./context/UserContext";
 import VisitedProvider from "./context/VisitedContext";
+
+if (import.meta.env.NODE_ENV === "production") {
+    disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
