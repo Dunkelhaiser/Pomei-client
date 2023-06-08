fd575956f97ca8e2321b6fbf49d8fd912b12dfd6
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-06-08 12:43:16 +0300

    Fixed welcome layout

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -9,24 +9,32 @@ const Welcome = () => {
     const navigate = useNavigate();
     const { setVisited } = useContext(VisitedContext);
     return (
-        <Layout className={WelcomeStyles.welcome} type="centered">
-            <h1 className={WelcomeStyles.title}>Welcome to Pomei!</h1>
-            <p className={WelcomeStyles.content}>
-                Sign up to save and access your notes from any device, and enjoy additional features like folders, archive, bin and pinning
-                notes.
-            </p>
-            <div className={WelcomeStyles.buttons}>
-                <Button
-                    label="Skip"
-                    color="neutral"
-                    styleType="text"
-                    onClick={() => {
-                        navigate("/home");
-                        setVisited();
-                    }}
-                />
-                <Button label="Sign Up" onClick={() => navigate("/sign_in")} />
-            </div>
+        <Layout type="centered">
+            <section className={WelcomeStyles.welcome}>
+                <h1 className={WelcomeStyles.title}>Welcome to Pomei!</h1>
+                <p className={WelcomeStyles.content}>
+                    Sign up to save and access your notes from any device, and enjoy additional features like folders, archive, bin and
+                    pinning notes.
+                </p>
+                <div className={WelcomeStyles.buttons}>
+                    <Button
+                        label="Skip"
+                        color="neutral"
+                        styleType="text"
+                        onClick={() => {
+                            navigate("/home");
+                            setVisited();
+                        }}
+                    />
+                    <Button
+                        label="Sign Up"
+                        onClick={() => {
+                            navigate("/sign_in");
+                            setVisited();
+                        }}
+                    />
+                </div>
+            </section>
         </Layout>
     );
 };
