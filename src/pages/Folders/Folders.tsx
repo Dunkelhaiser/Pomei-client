7bb9e4910647969e9ef6f71a8280407bd74c6472
KaeserOfHonour <kaeser418@gmail.com>
2023-07-06 20:16:06 +0300

    Added folders list from server

--- src/pages/Folders/Folders.tsx
+++ src/pages/Folders/Folders.tsx
@@ -1,20 +1,36 @@
+import { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Folder from "../../components/Folder/Folder";
 import Layout from "../../components/Layout/Layout";
 import FloatingIcon from "../../components/FloatingIcon/FloatingIcon";
+import { getFolders } from "../../api/folders";
+import { UserContext } from "../../context/UserContext";
+import Loader from "../../components/Loader/Loader";
+import Text from "../../components/Text/Text";
 
 const Folders = () => {
+    const { isAuthorized } = useContext(UserContext);
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["folders"],
+        queryFn: () => getFolders(1, 15, "desc", "updatedAt"),
+        enabled: isAuthorized,
+    });
     return (
         <>
             <FloatingIcon icon={faPlus} />
             <Layout title="Folders" type="grid">
-                <Folder title="My Folder" />
-                <Folder title="Homework" color="purple" />
-                <Folder title="Secret" color="green" />
-                <Folder title="Copy" />
-                <Folder title="Copy" color="grey" />
-                <Folder title="Copy" color="teal" />
-                <Folder title="Copy" />
+                {isLoading && <Loader />}
+                {isError && <Text text="Failed to load notes." type="p" />}
+                {!isLoading &&
+                    !isError &&
+                    (data?.folders?.length > 0 ? (
+                        data?.folders?.map((folder) => {
+                            return <Folder key={folder.id} title={folder.title} color={folder.color} />;
+                        })
+                    ) : (
+                        <Text text="No notes found." type="p" />
+                    ))}
             </Layout>
         </>
     );
